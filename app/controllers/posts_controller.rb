1e3d664446949a4367d05620e2358c145bc4196c
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-07-31 20:20:51 +0900

    メモ投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   
   def create
     post = Post.create(content: params[:content], checked: false)
-    redirect_to action: :index
+    render json:{ post: post }
   end
 
   def checked
