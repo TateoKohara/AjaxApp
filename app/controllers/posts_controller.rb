d88b5dee3402a2addac719d238f6f50fa0a2d2e9
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-07-31 19:05:50 +0900

    フロント側の実装終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,26 @@ class PostsController < ApplicationController
   
   def create
     Post.create(content: params[:content])
+<<<<<<< Updated upstream
+=======
+    redirect_to action: :index
+  end
+
+  def checked
+
+  
+
+
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+>>>>>>> Stashed changes
   end
 
 end
---
+++ app/javascript/checked.js
@@ -0,0 +1,41 @@
+function check() {
+  const posts = document.getElementsByClassName("post");
+  postsA = Array.from(posts);　// 取得したDOMを配列に変換している
+
+  postsA.forEach(function (post) { 
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");// 投稿をクリックした場合に実行する処理を定義している
+    post.addEventListener("click", (e) => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);//リクエストの初期化
+      XHR.responseType = "json";//レスポンスとして欲しい情報の形式を指定
+      XHR.send();//リクエストを送信、openメソッドで非同期通信をtrueにしてるのですぐレスポンスされる
+      XHR.onload = () => {　// レスポンスを受け取った時の処理を記述する
+        const item = XHR.response.post;
+        if (item.checked === true){
+          post.setAttribute("data-check", "true");// 既読状態であれば、灰色に変わるcssを適用するためのカスタムデータを追加している
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");　// 未読状態であれば、カスタムデータを削除している
+        }
+        if (XHR.status != 200) {    //レスポンスがエラーだった時の記述
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        } else {
+          return null;
+        }
+      }
+      XHR.onerror = () => {    //リクエストの送信が失敗だった時の記述
+        alert("Request failed");
+      };
+
+      e.preventDefault(); //イベントバンドラー実行後イベントをキャンセルする記述
+
+     });
+  });
+}
+
+setInterval(check,1000); //check関数が1秒に1度実行される
+
+window.addEventListener("load", check);
