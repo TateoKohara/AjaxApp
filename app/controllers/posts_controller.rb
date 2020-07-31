5e014e595d9c5beca1c4e18f2669c675acc0fd9f
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-07-31 20:18:27 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,27 @@
 class PostsController < ApplicationController
 
   def index   #indexというインスタンスメソッドを定義した
-    @posts = Post.all 
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  
   
   def create
-    Post.create(content: params[:content])
+    post = Post.create(content: params[:content], checked: false)
+    redirect_to action: :index
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
+
+
